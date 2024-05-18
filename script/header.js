e6b06c617bede4c8c28d25905afa6c4016db6388
Raeval <mhad2934@uni.sydney.edu.au>
2024-05-18 15:33:57 +1000

    User can now choose the language

--- script/header.js
+++ script/header.js
@@ -1,6 +1,20 @@
-const nav = document.querySelector(".navbar");
-fetch('./components/navbar.html')
-  .then(res => res.text())
-  .then(data => {
-    nav.innerHTML = data;
-  })
+document.addEventListener('DOMContentLoaded', (event) => {
+  const language = document.getElementById('language');
+  const languageOptions = document.getElementById('language-options');
+
+  language.addEventListener('mouseenter', () => {
+      languageOptions.style.display = 'flex';
+  });
+
+  language.addEventListener('mouseleave', () => {
+      languageOptions.style.display = 'none';
+  });
+
+  languageOptions.addEventListener('mouseenter', () => {
+      languageOptions.style.display = 'flex';
+  });
+
+  languageOptions.addEventListener('mouseleave', () => {
+      languageOptions.style.display = 'none';
+  });
+});
--- script/main_elderly.js
+++ script/main_elderly.js
@@ -1,10 +1,3 @@
-const nav = document.querySelector(".navbar");
-fetch('./components/navbar.html')
-  .then(res => res.text())
-  .then(data => {
-    nav.innerHTML = data;
-  })
-
 const schedule = document.querySelector(".schedule");
 fetch('./components/schedule.html')
   .then(res => res.text())
